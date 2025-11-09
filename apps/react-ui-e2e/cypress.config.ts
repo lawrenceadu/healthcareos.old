ada1cafeef5b925f63f19c02e022e6e767392367
Lawrence Adu <larrybuntus@gmail.com>
2025-11-09 23:50:54 +0000

    Upgrade: nx upgraded to v20 successfully

--- apps/react-ui-e2e/cypress.config.ts
+++ apps/react-ui-e2e/cypress.config.ts
@@ -2,5 +2,10 @@ import { defineConfig } from 'cypress';
 import { nxE2EStorybookPreset } from '@nx/storybook/presets/cypress';
 
 export default defineConfig({
-  e2e: nxE2EStorybookPreset(__dirname),
+  e2e: {
+    ...nxE2EStorybookPreset(__dirname),
+    // Please ensure you use `cy.origin()` when navigating between domains and remove this option.
+    // See https://docs.cypress.io/app/references/migration-guide#Changes-to-cyorigin
+    injectDocumentDomain: true,
+  },
 });
